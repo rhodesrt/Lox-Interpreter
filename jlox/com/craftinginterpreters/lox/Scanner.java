9e48cb92e9fe5ecfbfa2089122b677499e92e5dc
Tristan Rhodes <rhodestrt@gmail.com>
2023-07-04 15:42:19 -0700

    Finished Scanner Implementation. Added support for block /* ... */ comments.

--- jlox/com/craftinginterpreters/lox/Scanner.java
+++ jlox/com/craftinginterpreters/lox/Scanner.java
@@ -13,6 +13,27 @@ class Scanner {
   private int start = 0;
   private int current = 0;
   private int line = 1;
+  private static final Map<String, TokenType> keywords;
+
+  static {
+    keywords = new HashMap<>();
+    keywords.put("and",    AND);
+    keywords.put("class",  CLASS);
+    keywords.put("else",   ELSE);
+    keywords.put("false",  FALSE);
+    keywords.put("for",    FOR);
+    keywords.put("fun",    FUN);
+    keywords.put("if",     IF);
+    keywords.put("nil",    NIL);
+    keywords.put("or",     OR);
+    keywords.put("print",  PRINT);
+    keywords.put("return", RETURN);
+    keywords.put("super",  SUPER);
+    keywords.put("this",   THIS);
+    keywords.put("true",   TRUE);
+    keywords.put("var",    VAR);
+    keywords.put("while",  WHILE);
+  }
 
   Scanner(String source) {
     this.source = source;
@@ -41,7 +62,11 @@ class Scanner {
       case '-': addToken(MINUS); break;
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
-      case '*': addToken(STAR); break;
+      case '*':
+        if (!match('/')) {
+          addToken(STAR); break;
+        }
+        break;
       case '!':
         addToken(match('=') ? BANG_EQUAL : BANG);
         break;
@@ -58,6 +83,13 @@ class Scanner {
         if (match('/')) {
           // A comment goes until the end of the line.
           while (peek() != '\n' && !isAtEnd()) advance();
+        } else if (match('*')) {
+          while (peek() != '*' && peek() != '\n' && !isAtEnd()) {
+            if (peek() == '\n') {
+              line++;
+            }
+            advance();
+          }
         } else {
           addToken(SLASH);
         }
@@ -67,15 +99,68 @@ class Scanner {
       case '\t':
         // Ignore whitespace.
         break;
-
       case '\n':
         line++;
         break;
+      case '"': string(); break;
 
       default:
-        Lox.error(line, "Unexpected character.");
+        if (isDigit(c)) {
+          number();
+        } else if (isAlpha(c)) {
+          identifier();
+        } else {
+          Lox.error(line, "Unexpected character.");
+        }
         break;
     }
+  }
+
+  private void identifier() {
+    while (isAlphaNumeric(peek())) advance();
+
+    String text = source.substring(start, current);
+    TokenType type = keywords.get(text);
+    if (type == null) type = IDENTIFIER;
+    addToken(type);
+  }
+
+  private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+      // Consume the "."
+      advance();
+
+      while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,
+        Double.parseDouble(source.substring(start, current)));
+  }
+
+  private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
+  } 
+
+  private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+      if (peek() == '\n') line++;
+      advance();
+    }
+
+    if (isAtEnd()) {
+      Lox.error(line, "Unterminated string.");
+      return;
+    }
+
+    // The closing ".
+    advance();
+
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
   }
 
   private boolean match(char expected) {
@@ -89,6 +174,21 @@ class Scanner {
   private char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+  }
+
+  private char peekNext() {
+    if (current + 1 >= source.length()) return '\0';
+    return source.charAt(current + 1);
+  }
+
+  private boolean isAlpha(char c) {
+    return (c >= 'a' && c <= 'z') ||
+           (c >= 'A' && c <= 'Z') ||
+            c == '_';
+  }
+
+  private boolean isAlphaNumeric(char c) {
+    return isAlpha(c) || isDigit(c);
   }
 
   private boolean isAtEnd() {
